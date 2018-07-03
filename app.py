5f431e514c7c01d0ebeeda91ed3f0caf9b847ff0
DrLusine <34442296+DrLusine@users.noreply.github.com>
2018-07-03 15:59:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sklearn.externals import joblib
 from flask_cors import CORS
 
 
-my_model = joblib.load("./LM_33%_split_model_python3.pkl")
+model = joblib.load("./LM_33%_split_model_python3.pkl")
 
 #creating web service running on port 8000, answer POST requests
 app = Flask(__name__)
@@ -19,13 +19,13 @@ def make_predict():
         try:
             #expecting user imput as a json file with a title 'volume'
             data = request.get_json()
-            user_data=data["volume"]
+            user_data = request.args.get('volume')
                            
         except ValueError:
             return jsonify("error text here")
 
         #return a single prediction and convert to json
-        return jsonify({'cost':my_model.predict(user_data).tolist()})
+        return jsonify({'cost':model.predict(user_data).tolist()})
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
