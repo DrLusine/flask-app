cfcbf46640333d77e2c7b9f38ff973298e1e4816
DrLusine <34442296+DrLusine@users.noreply.github.com>
2018-07-03 12:12:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask,abort,jsonify,request
 import numpy as np
 from sklearn.externals import joblib
 
+
+my_model = joblib.load("./LM_33%_split_model.pkl")
+
 #creating web service running on port 8000, answer POST requests
 app = Flask(__name__)
-
 @app.route("/api", methods=['POST'])
 
 #prediction function
@@ -15,9 +17,9 @@ def make_predict():
             #expecting user imput as a json file with a title 'volume'
             data = request.get_json()
             user_data=data["volume"]
-            my_model = joblib.load("./LM_33%_split_model.pkl")
+            
         except ValueError:
-            return jsonify("errors text here")
+            return jsonify("error text here")
 
         #return a single prediction and convert to json
         return jsonify(my_model.predict(user_data).tolist())
@@ -32,8 +34,6 @@ def make_predict():
    ### output=[y[0]]
 
     ###return jsonify(results=output)
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
