21079a44cfd1d9355f4679ee67fc718b310b5bf2
DrLusine <34442296+DrLusine@users.noreply.github.com>
2018-07-03 15:45:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,36 +10,22 @@ my_model = joblib.load("./LM_33%_split_model_python3.pkl")
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "https://testing-cost-predictor.firebaseapp.com"}})
 
-@app.route("/api", methods=['POST'])
+@app.route("/fitOutCostPrediction", methods=['GET'])
 
 #prediction function
 def make_predict():
 
-    if request.method =='POST':
+    if request.method =='GET':
         try:
-            
             #expecting user imput as a json file with a title 'volume'
             data = request.get_json()
-            
             user_data=data["volume"]
-            
-            
+                           
         except ValueError:
             return jsonify("error text here")
 
         #return a single prediction and convert to json
-        return jsonify(my_model.predict(user_data).tolist())
-
-###second test version
-    ###data=request.get_json(force=True)
-
-   ### user_data = [data['volume']]
-    ###predict_request = np.array(user_data)
-    ### my_model = joblib.load("LM_33%_split_model.pkl")
-   ### y=my_model(predict_request)
-   ### output=[y[0]]
-
-    ###return jsonify(results=output)
-
+        return jsonify({'cost':my_model.predict(user_data).tolist()})
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
