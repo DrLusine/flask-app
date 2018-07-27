c0f895879aa855ce3ceb907df8dfe39c030237a9
Simon Norman <01secn@gmail.com>
2018-07-27 18:21:15 +0100

    Completely moved get cost prediction logic into own resource file

--- app.py
+++ app.py
@@ -1,48 +1,22 @@
-from flask import Flask,abort,jsonify,request,json
+from flask import Flask, abort, jsonify, request, json
 import numpy as np
 from sklearn.externals import joblib
 from flask_cors import CORS
+from resources.fit_out_cost_prediction import fitout_cost_prediction_api
 
+DEBUG = True
+HOST = '0.0.0.0'
+PORT = 8080
 
-model = joblib.load("./LR_33%split_model_inc_CatAB.pkl")
+app = Flask(__name__)
 
-#creating web service running on port 8000, answer POST requests
-app = Flask(__name__)   
+app.register_blueprint(fitout_cost_prediction_api)
+
 cors = CORS(app, resources={
-  r"/*": {
-    "origins": ["https://testing-cost-predictor.firebaseapp.com","http://localhost:9000", "https://frontend-cost-predictor-ac557.firebaseapp.com"]
-  }
+    r"/*": {
+        "origins": ["https://testing-cost-predictor.firebaseapp.com", "http://localhost:9000", "https://frontend-cost-predictor-ac557.firebaseapp.com"]
+    }
 })
 
-@app.route("/fitOutCostPrediction", methods=['POST'])
-
-#prediction function
-def make_predict():
-
-    if request.method =='POST':
-        try:
-            costPredictionParameters = request.get_json()
-            buildingVolume = costPredictionParameters['buildingVolume']
-            isCatAIncluded = 0
-            isCatBIncluded = 0
-            isCatAAndBIncluded = 0
-
-            if costPredictionParameters['isCatAIncluded'] == True and costPredictionParameters['isCatBIncluded'] == True:
-                isCatAAndBIncluded = 1
-            elif costPredictionParameters['isCatAIncluded'] == True and costPredictionParameters['isCatBIncluded'] == False:
-                isCatAIncluded = 1
-            elif costPredictionParameters['isCatAIncluded'] == False and costPredictionParameters['isCatBIncluded'] == True:
-                isCatBIncluded = 1
-            
-            costPredictionParametersForModel = [buildingVolume, isCatAIncluded, isCatBIncluded, isCatAAndBIncluded]
-            
-            cost = model.predict([costPredictionParametersForModel])[0]
-       
-        except ValueError:
-            return jsonify("error text here")
-        
-        # return a json value
-        return json.dumps({'cost':cost})
-    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(debug=DEBUG, host=HOST, port=PORT)
--- resources/fit_out_cost_prediction.py
+++ resources/fit_out_cost_prediction.py
@@ -1,30 +1,38 @@
 
-from flask import jsonify, Blueprint, json
+from flask import jsonify, Blueprint, json, request, json
+
 from flask_restful import Resource, Api
 from sklearn.externals import joblib
 
-model = joblib.load("./LR_33%split_model_inc_CatAB.pkl")
+model = joblib.load("./deep_learning_models/LR_33%split_model_inc_CatAB.pkl")
 
 
 class FitOutCostPrediction(Resource):
-    def post(self, id):
-        costPredictionParameters = request.get_json()
-        buildingVolume = costPredictionParameters['buildingVolume']
-        isCatAIncluded = 0
-        isCatBIncluded = 0
-        isCatAAndBIncluded = 0
+    def post(self):
+        try:
+            costPredictionParameters = request.get_json()
+            buildingVolume = costPredictionParameters['buildingVolume']
+            isCatAIncluded = 0
+            isCatBIncluded = 0
+            isCatAAndBIncluded = 0
 
-    if costPredictionParameters['isCatAIncluded'] == True and costPredictionParameters['isCatBIncluded'] == True:
-        isCatAAndBIncluded = 1
-    elif costPredictionParameters['isCatAIncluded'] == True and costPredictionParameters['isCatBIncluded'] == False:
-        isCatAIncluded = 1
-    elif costPredictionParameters['isCatAIncluded'] == False and costPredictionParameters['isCatBIncluded'] == True:
-        isCatBIncluded = 1
+            if costPredictionParameters['isCatAIncluded'] == True and costPredictionParameters['isCatBIncluded'] == True:
+                isCatAAndBIncluded = 1
+            elif costPredictionParameters['isCatAIncluded'] == True and costPredictionParameters['isCatBIncluded'] == False:
+                isCatAIncluded = 1
+            elif costPredictionParameters['isCatAIncluded'] == False and costPredictionParameters['isCatBIncluded'] == True:
+                isCatBIncluded = 1
 
-    costPredictionParametersForModel = [
-        buildingVolume, isCatAIncluded, isCatBIncluded, isCatAAndBIncluded]
-    cost = model.predict([costPredictionParametersForModel])[0]
-    return json.dumps({'cost': cost})
+            costPredictionParametersForModel = [
+                buildingVolume, isCatAIncluded, isCatBIncluded, isCatAAndBIncluded]
+
+            cost = model.predict([costPredictionParametersForModel])[0]
+
+        except ValueError:
+            return jsonify("error text here")
+
+        # return a json value
+        return json.dumps({'cost': cost})
 
 
 fitout_cost_prediction_api = Blueprint(
@@ -34,5 +42,5 @@ api = Api(fitout_cost_prediction_api)
 api.add_resource(
     FitOutCostPrediction,
     '/fitOutCostPrediction',
-    endpoint='courses'
+    endpoint='fitOutCostPrediction'
 )
