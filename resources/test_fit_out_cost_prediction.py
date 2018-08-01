97b0ad12490e715cbac259f9f8dccdd2dc0d1208
Simon Norman <01secn@gmail.com>
2018-08-01 11:24:19 +0100

    Fixture converted to full test client factory

--- resources/test_fit_out_cost_prediction.py
+++ resources/test_fit_out_cost_prediction.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify
-from numbers import Number
 from resources.fit_out_cost_prediction import fitout_cost_prediction_api
+from numbers import Number
 import pytest
 
 
-def test_get_fit_out_cost_prediction(app_instance):
-        app_instance.register_blueprint(fitout_cost_prediction_api)
-        client = app_instance.test_client()
+def test_get_fit_out_cost_prediction(test_client_factory):
+        client = test_client_factory(fitout_cost_prediction_api)
+
         response = client.post('/fitOutCostPrediction', json={
         'buildingVolume': 10000, 'isCatAIncluded': False, 'isCatBIncluded': True
-    })
+        })
+        
         assert response.status_code == 200
         response_data = response.get_json()
         assert isinstance(response_data['cost'], Number)
