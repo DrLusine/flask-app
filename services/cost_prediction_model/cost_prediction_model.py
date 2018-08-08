5bc62f3816be0fc16a16bd4b3c2743f3c041ae06
DrLusine <34442296+DrLusine@users.noreply.github.com>
2018-08-08 16:35:47 +0100

    Update cost_prediction_model.py

--- services/cost_prediction_model/cost_prediction_model.py
+++ services/cost_prediction_model/cost_prediction_model.py
@@ -4,7 +4,7 @@ from sklearn.externals import joblib
 class CostPredictionModel:
     def __init__(self):
        self.costPredictionModel = joblib.load(
-          "./services/cost_prediction_model/LR_33%split_model_inc_CatAB.pkl")
+          "./services/cost_prediction_model/LR_split_model_MASTER_8thAugust.pkl")
 
     def predictCost(self, costPredictionParametersForModel):
         return self.costPredictionModel.predict(
